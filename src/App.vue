898c3c327f2375abfaf53efbb077139dc30d0296
Jan Eberlein <code@janeberlein.de>
2024-02-16 13:13:32 +0100

    adds dark mode

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,31 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-</script>
+import DarkModeSelector from '@/components/DarkModeSelector.vue'
+</script> 
 
 <template>
-  <header>
-    <div>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+  <div
+    class="w-screen h-screen bg-gradient-to-b from-slate-200 to-zinc-200 dark:from-slate-800 dark:to-zinc-800"
+  >
+    <header class="flex h-10 max-w-md w-full">
+      <div>
+        <nav>
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/about">About</RouterLink>
+        </nav>
+      </div>
+    </header>
 
-  <RouterView />
+    <DarkModeSelector> </DarkModeSelector>
+
+
+
+    <RouterView />
+  </div>
 </template>
+
+<style>
+html.dark {
+  color-scheme: dark;
+}
+</style>
---
+++ src/components/DarkModeSelector.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { useDark, useToggle } from '@vueuse/core'
+
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+</script>
+
+<template>
+  <div>
+    <button @click="toggleDark()">Dark Mode</button>
+  </div>
+</template>
