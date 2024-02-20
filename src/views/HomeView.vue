943bd17a56ad739d02470aeeb2bd5eec5da788d4
Jan Eberlein <code@janeberlein.de>
2024-02-20 14:00:47 +0100

    adds markdown render capabilities

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,20 @@
 <script setup>
 import CardItem from '@/components/CardItem.vue'
+import markdownit from 'markdown-it'
+import cheatSheet from '@/content/cheatSheet.md?raw'
+
+const md = markdownit()
+const result = md.render(cheatSheet)
 </script>
 
 <template>
-  <main>Main</main>
-  <CardItem> Test </CardItem>
+  <main class="flex flex-col space-y-4 pb-4">
+    <CardItem class="basis-1">
+      <p>Main</p>
+    </CardItem>
+    
+    <CardItem class="basis-9/12">
+      <p v-html="result" class=""></p>
+    </CardItem>
+  </main>
 </template>
