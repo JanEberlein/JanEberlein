328d1fbc88462fb346d72ccb296fe274a21838f8
Jan Eberlein <code@janeberlein.de>
2024-02-22 17:04:05 +0100

    beautifies 💄

--- src/views/CheatSheetView.vue
+++ src/views/CheatSheetView.vue
@@ -10,7 +10,10 @@ const result = md.render(cheatSheet)
 <template>
   <div class="h-full flex flex-col space-y-4">
     <CardItem class="grow bg-none p-0">
-      <p v-html="result" class="prose prose-amber dark:prose-invert max-w-full bg-gradient-dotted p-4"></p>
+      <p
+        v-html="result"
+        class="prose prose-amber dark:prose-invert max-w-full bg-gradient-dotted p-4"
+      ></p>
     </CardItem>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,9 @@
 <script setup>
 import CardItem from '@/components/CardItem.vue'
-
 </script>
 
 <template>
   <div class="h-full w-full flex flex-col space-y-4">
-    <CardItem class="">
-      Main
-    </CardItem>
+    <CardItem class=""> Main </CardItem>
   </div>
 </template>
