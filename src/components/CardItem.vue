c5b9640637802fee84b8988c4281556a5623bf7a
Jan Eberlein <code@janeberlein.de>
2024-02-26 16:28:54 +0100

    beautifies card background image

--- src/components/CardItem.vue
+++ src/components/CardItem.vue
@@ -1,7 +1,31 @@
-<script setup></script>
+<script setup>
+import { computed } from 'vue';
+
+  const props = defineProps({
+  backgroundImage: {
+    type: String,
+    required: false,
+    default: ''
+  }
+})
+
+const backgroundSource = computed(() => {
+  if(!props.backgroundImage) return
+  const path = `/src/assets/${props.backgroundImage}`
+  const assets = import.meta.glob("/src/assets/*", { eager: true })
+  const result = assets[path]
+  if (!result) return
+  return result.default
+})
+</script>
 
 <template>
-  <div class="box-rounded h-20 w-full bg-gradient-dotted from-slate-400 overflow-scroll">
-    <slot></slot>
+  <div class="relative group min-h-20 w-full p-0 box-rounded from-slate-400 overflow-scroll">
+    <img v-if="props.backgroundImage" :src="backgroundSource" alt="" 
+      class="w-full h-full opacity-20 group-hover:opacity-50 object-cover"
+    >
+    <div class="absolute flex flex-col justify-between top-0 left-0 w-full h-full p-2 px-4 pb-3 bg-gradient-dotted">
+      <slot></slot>
+    </div>
   </div>
 </template>
--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -8,21 +8,16 @@ entries.value = projects.entries
 entries.value.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())
 //entries.value.reverse()
 
-function backgroundURL(imageName) {
-  if(!imageName) return
-  const path = `/src/assets/${imageName}`
-  const assets = import.meta.glob("/src/assets/*", { eager: true })
-  const result = assets[path]
-  if (!result) return
-  return result.default
-}
+
 
 </script>
 
 <template>
   <div class="h-full w-full flex flex-col space-y-4">
-    <CardItem v-for="entry in entries" :key="entry.key" class="h-52 flex flex-col justify-between pb-3" :class="entry.styling">
-      <h1 class="text-4xl font-extrabold text-right grow" :style="{backgroundImage: `url('${backgroundURL(entry.backgroundImage)}')`}">{{ entry.title }}</h1>
+    <CardItem v-for="entry in entries" :key="entry.key" :background-image="entry.backgroundImage" class="h-52">
+      <h1 class="text-4xl font-extrabold text-right grow">
+        {{ entry.title }}
+      </h1>
       <p>{{ entry.description }}</p>
       <p class="text-right text-slate-400">{{ new Date(entry.date).toLocaleDateString('de-DE') }}</p>
     </CardItem>
